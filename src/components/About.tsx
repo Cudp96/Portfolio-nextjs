d4dbd7b17f89d087f3d8960f0de727ce585cfba6
Sudeep <sudeepstha.96@gmail.com>
2024-02-26 16:20:11 +0545

    feat: bug

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,9 +4,9 @@ export default function About() {
   return (
     <section
       id="about"
-      className="h-[32rem] md:min-h-screen bg-neutral-950 relative flex flex-col items-center justify-center antialiased"
+      className="h-[40rem] md:min-h-screen bg-neutral-950 relative flex flex-col items-center justify-center antialiased my-4"
     >
-      <div className="max-w-5xl mx-auto p-4">
+      <div className="max-w-5xl mx-auto p-8">
         <h1 className="relative z-10 text-2xl md:text-5xl lg:text-6xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
           Hi,I am <span className="text-violet-400">Sudeep Shrestha</span> from
           Nepal.
