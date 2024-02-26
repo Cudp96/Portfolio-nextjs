dc5fffc21fd1135a2102eb1ded36485b1b15432b
Sudeep <sudeepstha.96@gmail.com>
2024-02-26 16:08:16 +0545

    feat; bug fixed in about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,14 +4,14 @@ export default function About() {
   return (
     <section
       id="about"
-      className=" h-[32rem] md:min-h-screen bg-neutral-950 relative flex flex-col items-center justify-center antialiased"
+      className="h-[32rem] md:min-h-screen bg-neutral-950 relative flex flex-col items-center justify-center antialiased"
     >
       <div className="max-w-5xl mx-auto p-4">
         <h1 className="relative z-10 text-2xl md:text-5xl lg:text-6xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
           Hi,I am <span className="text-violet-400">Sudeep Shrestha</span> from
           Nepal.
         </h1>
-        <p className="text-neutral-400 max-w-5xl text-xl mx-auto my-2 text-md text-justify relative z-10">
+        <p className="text-neutral-400  text-sm sm:text-xl mx-auto my-2 text-justify relative z-10">
           <span className="text-2xl font-bold">I</span> am aspiring front-end
           developer with a focus on creating user-friendly and visually
           appealing web experiences. I am passionate about learning and
